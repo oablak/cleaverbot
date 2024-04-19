7c56a83beb5a0a558cf9d554d5f22658b41d7674
oguza <oablak@gmail.com>
2024-04-19 14:36:21 +0300

    Sepeak support commented

--- clever_bot/main.py
+++ clever_bot/main.py
@@ -4,8 +4,23 @@ from gtts import gTTS
 from random import randint
 from playsound import playsound
 import os
+import speech_recognition as sr
+
 
 translator = Translator()
+rec = sr.Recognizer()
+
+def record():
+    with sr.Microphone() as source:
+        voice_rec = ""
+        audio = rec.listen(source, 5, 5)
+        try:
+            voice_rec = rec.recognize_amazon(audio)
+        except:
+            speak("Anlamadım")
+
+    return voice_rec
+
 
 def speak(text):
     tts = gTTS(text)
@@ -24,11 +39,13 @@ def chat(bot, user_prompt, bot_prompt):
     while True:
         user_input = input(user_prompt)
         user_input_en = translate(user_prompt, user_input, "en")
+        #print(user_prompt, user_input_en)
         if user_input == "q":
             break
         reply = bot.single_exchange(user_input_en)
+        #print(bot_prompt, reply)
         reply_tr = translate(bot_prompt, reply, "tr")
-        speak(reply_tr)
+        #speak(reply_tr)
         print(bot_prompt, reply_tr)
     bot.close()
 
