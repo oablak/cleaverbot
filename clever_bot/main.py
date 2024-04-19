b1a6141f18b88e3e7b8e36dee63cdbe34567ac74
oguza <oablak@gmail.com>
2024-04-19 13:15:12 +0300

    Speak support added to bot

--- clever_bot/main.py
+++ clever_bot/main.py
@@ -1,7 +1,19 @@
 from cleverbotfree import Cleverbot
 from googletrans import Translator
+from gtts import gTTS
+from random import randint
+from playsound import playsound
+import os
 
 translator = Translator()
+
+def speak(text):
+    tts = gTTS(text)
+    rand = randint(1, 100000)
+    file = "audio." + str(rand) + ".mp3"
+    tts.save(file)
+    playsound(file)
+    os.remove(file)
 
 def translate(who, text, lang):
     translated = translator.translate(text, dest=lang)
@@ -16,6 +28,7 @@ def chat(bot, user_prompt, bot_prompt):
             break
         reply = bot.single_exchange(user_input_en)
         reply_tr = translate(bot_prompt, reply, "tr")
+        speak(reply_tr)
         print(bot_prompt, reply_tr)
     bot.close()
 
